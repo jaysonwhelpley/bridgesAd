c9d6503e54145479d251982bacb215110d48ba81
Jayson Whelpley <jayson.whelpley@gmail.com>
2018-02-21 17:03:39 -0500

    remove access keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,8 +6,8 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => 'AKIAJHI3BRVWVFS774SA',
-      :aws_secret_access_key => '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'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
